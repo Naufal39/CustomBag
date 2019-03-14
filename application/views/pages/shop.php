3150bd28fb27bd640d627e4fb75e3b363a4a6185
unknown <naufalsetyanto39@gmail.com>
2019-03-15 00:05:01 +0700

    up

--- application/views/pages/shop.php
+++ application/views/pages/shop.php
@@ -41,10 +41,10 @@
               <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
                 <div class="block-4 text-center border">
                   <figure class="block-4-image">
-                    <a href="<?php base_url('view/singshop');?>"><img src="<?php echo base_url('assets/home/images/cloth_1.jpg');?>" alt="Image placeholder" class="img-fluid"></a>
+                    <a href="<?php echo base_url('view/single');?>"><img src="<?php echo base_url('assets/home/images/cloth_1.jpg');?>" alt="Image placeholder" class="img-fluid"></a>
                   </figure>
                   <div class="block-4-text p-4">
-                    <h3><a href="<?php base_url();?>single">Tank Top</a></h3>
+                    <h3><a href="<?php echo base_url();?>view/single">Tank Top</a></h3>
                     <p class="mb-0">Finding perfect t-shirt</p>
                     <p class="text-primary font-weight-bold">$50</p>
                   </div>
